667c907f8cac4e753e810c7c6eaa3f4ea4848e5d
Zi0LEO <leonardo.npl.02@gmail.com>
2024-05-03 13:26:41 +0200

    Added new methods to support Huffman algorithm

--- graphAlgorithms.py
+++ graphAlgorithms.py
@@ -1,6 +1,7 @@
 import weightedGraphL as g
 import math
-
+import heap 
+import weightedTree as wTree
 
 def dijkstra(graph, start):
     size = g.size(graph)
@@ -26,3 +27,36 @@ def closestNode(dist, visited):
             node = i
             currentMinDistance = dist[i]
     return node
+
+def copyList(list):
+    copy = []
+    for i in range(len(list) - 1):
+        copy.append(list[i])
+    return copy;
+
+
+def codeGeneration(tree, retList, partialCode):
+    if wTree.isLeaf(tree):
+        D = copyList(partialCode)
+        retList.append([wTree.key(tree), D])
+    else:
+        partialCode.append(0)
+        codeGeneration(wTree.leftSon(tree), retList, partialCode)
+        partialCode.pop()
+        partialCode.append(1)
+        codeGeneration(wTree.rightSon(tree), retList, partialCode)
+
+
+
+def huffman(L):
+    Q = heap.createHeap()
+    n = len(L)
+    for(char, frequency) in L:
+        T = wTree.leafNode(char, frequency)
+        heap.insertHeap(Q, T)
+    for i in range(n - 1):
+        A = heap.deleteMin(Q)
+        B = heap.deleteMin(Q)
+        C = wTree.tree('_',A,B,wTree.weight(A) + wTree.weight(B))
+        heap.insertHeap
+
---
+++ weightedTree.py
@@ -0,0 +1,22 @@
+
+
+def tree(key, leftSon, rightSon, weight):
+    return [[key, leftSon, rightSon], weight]
+
+def leafNode(key, weight):
+    return [[key, [],[]], weight]
+
+def leftSon(node):
+    return node[0][1]
+
+def rightSon(node):
+    return node[0][1]
+
+def weight(node):
+    return node[1]
+
+def isEmpty(node):
+    return node[1] == 0;
+
+def isLeaf(node):
+    return isEmpty(leftSon(node)) and isEmpty(rightSon(node))
